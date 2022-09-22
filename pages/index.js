df7f4f51b34ebd68952d092b74fef4f46935ea22
Ahmad Ataka <ataka.ahmad@gmail.com>
2022-09-22 14:59:31 +0700

    Try abstract

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,17 @@ export default function Home() {
       <main>
         <Header title="Soft Robotics Workshop at ICRA 2023!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Soft Robotics: Fusing function with structure
         </p>
+
+        <Header title="Abstract" />
+        <p className="description">
+          The advent of soft material technologies in robotics has brought many distinct advantages, such as inherent compliance, safety, high manoeuvrability, and reconfigurability. Along with these advantages, the past years have seen many interesting applications for soft robots in areas such as surgical robotics, extreme environments, and logistics and material handling. Despite these rapid developments, many challenges that are key to the widespread adoption and efficient utilization of novel soft material technologies are still outstanding.
+          A major challenge that lies at the core of the field of soft robots is the fusion and integration of different functions with the soft robot body. The ability of different structures in a soft robot to seamlessly carry out different functions, for example, an actuator that is also a self-sensing element or the backbone of a robot body, will produce numerous advantages. Such a fusion is underpinned by many aspects, such as design methodologies, the development of multifunctional components, manufacturing techniques to cofabricate functional structures, sensing and perception of data, and morphological computation.
+          The main objective of this workshop is to serve as a focal point for discussing this grand challenge of “Fusing Function with Structure in Soft Robotics’’. The workshop will host talks, short presentations, an Early Career Researchers Showcase and a panel discussion and will involve soft robotics researchers specializing in the broad domains of soft actuation, sensing, functional materials, manufacturing, and physical intelligence.
+        </p>
+
+        
       </main>
 
       <Footer />
